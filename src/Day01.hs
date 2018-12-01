eed91963369aaccfdcc946fd8f1f487fb9220028
Steve Purcell <steve@sanityinc.com>
2018-12-02 11:06:31 +1300

    1b

--- src/Day01.hs
+++ src/Day01.hs
@@ -3,12 +3,28 @@ module Day01
     ( main
     ) where
 
+import qualified Data.Set as Set
+import Data.Set (Set)
+
 readInt :: String -> Int
 readInt ('+':s) = read s
 readInt s = read s
 
+nums :: IO [Int]
+nums = fmap readInt . lines <$> readFile "input/1.txt"
+
 main :: IO ()
 main = do
-  nums :: [Int] <- fmap readInt . lines <$> readFile "input/1.txt"
-  print $ sum nums
+  ns <- nums
+  putStrLn "Part 1:"
+  print (sum ns)
+  putStrLn "Part 2:"
+  print (firstDup (scanl (+) 0 (cycle ns)))
 
+firstDup :: (Eq a, Ord a) => [a] -> Maybe a
+firstDup xs = go xs Set.empty
+  where
+    go [] _ = Nothing
+    go (y:ys) seen
+      | y `Set.member` seen = Just y
+      | otherwise = go ys (Set.insert y seen)
