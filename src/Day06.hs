e2ce3f828f4fa8749523370190445d5180d20c76
Steve Purcell <steve@sanityinc.com>
2018-12-07 14:47:44 +1300

    6b

--- src/Day06.hs
+++ src/Day06.hs
@@ -5,6 +5,8 @@ module Day06 where
 import Control.Arrow ((&&&), second)
 import Data.Foldable (all, minimum, minimumBy)
 import Data.Function (on)
+import qualified Data.Ix
+import qualified Data.Ix as Ix
 import Data.List (groupBy, maximum, minimum, nub, sort, sortOn)
 import Data.Map (Map)
 import qualified Data.Map as Map
@@ -23,27 +25,42 @@ manhattan a b = abs dx + abs dy
   where
     (V2 dx dy) = a - b
 
-bestAreaSize :: [Coord] -> Int
-bestAreaSize = maximum . fmap length . Map.elems . closests
+part1 :: [Coord] -> Int
+part1 = maximum . fmap length . Map.elems . closests
 
-closests :: [Coord] -> Map Coord [Coord]
-closests coords = Map.filter (all (not . isOnEdge)) distsMap
+part2 :: Int -> [Coord] -> Int
+part2 limit coords =
+  length
+    [c | c <- Ix.range (bounds coords), sum (manhattan c <$> coords) < limit]
+
+type Region = (Coord, Coord)
+
+bounds :: [Coord] -> Region
+bounds coords = (V2 minX minY, V2 maxX maxY)
   where
-    distsMap = Map.fromListWith (++) (second (: []) <$> dists)
-    dists =
-      [ (to, from)
-      | from <- V2 <$> [minX .. maxX] <*> [minY .. maxY]
-      , let ((closestDist, to):rest) =
-              sortOn fst $ (manhattan from &&& id) <$> coords
-      , closestDist `notElem` (fst <$> rest)
-      ]
     xs = [x | (V2 x _) <- coords]
     ys = [y | (V2 x y) <- coords]
     minX = minimum xs
     maxX = maximum xs
     minY = minimum ys
     maxY = maximum ys
-    isOnEdge (V2 x y) = x == minX || x == maxX || y == minY || y == maxY
+
+isOnEdge :: Region -> Coord -> Bool
+isOnEdge (V2 minX minY, V2 maxX maxY) (V2 x y) =
+  x == minX || x == maxX || y == minY || y == maxY
+
+closests :: [Coord] -> Map Coord [Coord]
+closests coords = Map.filter (all (not . isOnEdge region)) distsMap
+  where
+    region = bounds coords
+    distsMap = Map.fromListWith (++) (second (: []) <$> dists)
+    dists =
+      [ (to, from)
+      | from <- Ix.range region
+      , let ((closestDist, to):rest) =
+              sortOn fst $ (manhattan from &&& id) <$> coords
+      , closestDist `notElem` (fst <$> rest)
+      ]
 
 example :: [Coord]
 example = [V2 1 1, V2 1 6, V2 8 3, V2 3 4, V2 5 5, V2 8 9]
@@ -52,4 +69,6 @@ main :: IO ()
 main = do
   coords <- P.parseFile (parseCoords <* P.eof) "input/6.txt"
   putStrLn "Part 1:"
-  print (bestAreaSize coords)
+  print (part1 coords)
+  putStrLn "Part 2:"
+  print (part2 10000 coords)
